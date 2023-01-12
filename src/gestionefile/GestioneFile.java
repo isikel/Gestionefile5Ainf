c2ebcc496fa6797724c1449791e279b862bb45e1
Monica Ciuchetti <monica.ciuchetti@gmail.com>
2023-01-12 17:57:39 +0100

    primo commit con del progetto

---
+++ src/gestionefile/GestioneFile.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionefile;
+
+/**
+ *
+ * @author MC
+ * @version 12/01/23
+ */
+public class GestioneFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //1)LETTURA
+        Lettore lettore = new Lettore("user.json");
+        lettore.start();
+        //2)ELABORAZIONE
+        
+        //3) SCRITTURA
+        Scrittore scrittore = new Scrittore("output.csv");
+        Thread threadScrittore = new Thread(scrittore);
+        threadScrittore.start();
+    }
+    
+}
