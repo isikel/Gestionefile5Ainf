f6bcfe4829388fbe65313d04294e4c04e9d09e87
MovingN <giorgionucciarelli2@gmail.com>
2024-01-16 10:49:55 +0100

    Caricamento progetto iniziale.

--- src/gestionefile/GestioneFile.java
+++ src/gestionefile/GestioneFile.java
@@ -5,15 +5,20 @@ package gestionefile;
  * @author MC
  * @version 12/01/23
  */
+import java.util.Scanner;
 public class GestioneFile {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+         Scanner o = new Scanner(System.in);
         //1)LETTURA
         Lettore lettore = new Lettore("user.json");
+        System.out.println("Inserire username:");
+        String userName = o.nextLine();
+        System.out.println("\n Inserire password:");
+        String passKey = o.nextLine();
         lettore.start();
         //2)ELABORAZIONE
         
