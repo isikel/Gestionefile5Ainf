e8229bd88bbb0473f536199f07d3cc1997207452
Yassoss <yassine.meddiki@gmail.com>
2024-01-23 10:52:39 +0100

    invio 2

--- src/gestionefile/GestioneFile.java
+++ src/gestionefile/GestioneFile.java
@@ -5,22 +5,38 @@ package gestionefile;
  * @author MC
  * @version 12/01/23
  */
+   import java.util.Scanner;
 public class GestioneFile {
+ 
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Scanner ins = new Scanner(System.in);
         //1)LETTURA
         Lettore lettore = new Lettore("user.json");
         lettore.start();
         //2)ELABORAZIONE
+        System.out.println("inserisci username:");
+        String userName = ins.nextLine();
+        System.out.println("inserisci password:");
+        String password = ins.nextLine();
         
         //3) SCRITTURA
         Scrittore scrittore = new Scrittore("output.csv");
         Thread threadScrittore = new Thread(scrittore);
         threadScrittore.start();
+        Scrittore.scriviPsv();
+        FileInputStream inputStream = new FileInputStream(inFile);
+        FileOutputStream outputStream = new FileInputStream(outFile);
+        byte[] buffer = new byte[1024];
+        int bytesLetti;
+        while ((bytesLetti = inputStream.read(buffer)) != -1){
+            outputStream.write(buffer, 0, bytesLetti);
+        }
+        inputSTream.close();
+        
     }
     
 }
--- src/gestionefile/Scrittore.java
+++ src/gestionefile/Scrittore.java
@@ -19,6 +19,11 @@ public class Scrittore implements Runnable{
     public Scrittore(String nomeFile){
         this.nomeFile = nomeFile;
     }
+    public Scrittore(String nomeFile, String utente, String password){
+        this.nomeFile = nomeFile;
+        this.utente = utente;
+        this.password = password;
+    }
     
     @Override
     public void run() {
@@ -36,6 +41,32 @@ public class Scrittore implements Runnable{
                     new FileWriter(nomeFile));
             //2) scrivo nel buffer
             br.write("File in output");
+            br.write("\n\r");
+            //3) svuoto il buffer e salvo nel file i dati
+            br.flush();         
+        } catch (IOException ex) {
+            Logger.getLogger(Scrittore.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            if (br!=null)
+                try {
+                    //4)chiudo lo stream in uscita
+                    br.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Scrittore.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                
+        }
+    }
+        public void scrivi1(){
+        BufferedWriter br=null;
+        
+        try {
+            //1) apro il file
+            br = new BufferedWriter(
+                    new FileWriter(nomeFile));
+            //2) scrivo nel buffer
+            br.write(utente +";"+ password);
             br.write("\n\r");
             //3) svuoto il buffer e salvo nel file i dati
             br.flush();         
